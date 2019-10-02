f469e01cb9f90e2fdb226fd8cb8c5403d4a1318e
Uice Lu <uicestone@gmail.com>
2019-10-02 08:48:07 +0800

    import question cli

--- class.pbpark-cli.php
+++ class.pbpark-cli.php
@@ -3,7 +3,7 @@
 WP_CLI::add_command('pbpark', 'PB_Park_CLI');
 
 /**
- * Filter spam comments.
+ * 党建地图CLI工具
  */
 class PB_Park_CLI extends WP_CLI_Command {
 
@@ -72,6 +72,41 @@ class PB_Park_CLI extends WP_CLI_Command {
 			update_post_meta($shop->ID, 'longitude', $longitude);
 
 			WP_CLI::line( 'Location saved ' . $latitude . ',' . $longitude . ' ' . $shop->ID . ' ' . $shop->post_title . '.');
+		}
+	}
+
+	/**
+	 * Import questions
+	 */
+	public function import_questions() {
+		global $wpdb;
+		$questions = $wpdb->get_results("SELECT * FROM `questions`");
+		$points_questions = [];
+		foreach ($questions as $question) {
+			$existing_questions = get_posts(['title' => $question->title, 'post_type'=>'question']);
+
+			if ($existing_questions) {
+				$question_post_id = $existing_questions[0]->ID;
+			} else {
+				$question_post_id = wp_insert_post(array(
+					'post_title' => $question->title,
+					'post_type' => 'question',
+					'post_status' => 'publish'
+				));
+			}
+			$options = preg_split('/(\r?\n)+/', $question->options);
+			update_field('options', implode("\r\n", $options), $question_post_id);
+			update_field('true_option', $question->true_option, $question_post_id);
+			if (empty($points_questions[$question->point])) {
+				$points_questions[$question->point] = [];
+			}
+			WP_CLI::line('已导入题目：' . mb_substr($question->title, 0, 20) . '，点位：' . $question->point . '，题目ID：' . $question_post_id);
+			array_push($points_questions[$question->point], $question_post_id);
+		}
+		foreach ($points_questions as $point => $question_ids) {
+			$point_post = get_page_by_path($point, OBJECT, 'point');
+			update_field('questions', $question_ids, $point_post->ID);
+			WP_CLI::line('点位：' . $point_post->post_name . '设置了' . count($question_ids) . '道题目');
 		}
 	}
 
