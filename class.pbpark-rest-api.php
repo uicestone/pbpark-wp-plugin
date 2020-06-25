cf087fde3bffb2e2d9ef38a023b7cac66d856a9a
Uice Lu <uicestone@gmail.com>
2020-06-26 01:12:13 +0800

    term api with image

--- class.pbpark-rest-api.php
+++ class.pbpark-rest-api.php
@@ -3,6 +3,7 @@
 require_once(PB_Park__PLUGIN_DIR . 'rest-controllers/class.pbpark-rest-misc-controller.php');
 require_once(PB_Park__PLUGIN_DIR . 'rest-controllers/class.pbpark-rest-park-controller.php');
 require_once(PB_Park__PLUGIN_DIR . 'rest-controllers/class.pbpark-rest-post-controller.php');
+require_once(PB_Park__PLUGIN_DIR . 'rest-controllers/class.pbpark-rest-term-controller.php');
 
 class PB_Park_REST_API {
 
@@ -10,6 +11,7 @@ class PB_Park_REST_API {
 		(new PB_Park_REST_Misc_Controller())->register_routes();
 		(new PB_Park_REST_Park_Controller())->register_routes();
 		(new PB_Park_REST_Post_Controller())->register_routes();
+		(new PB_Park_REST_Term_Controller())->register_routes();
 	}
 
 }
---
+++ rest-controllers/class.pbpark-rest-term-controller.php
@@ -0,0 +1,42 @@
+<?php
+
+class PB_Park_REST_Term_Controller extends WP_REST_Controller {
+
+	public function __construct() {
+		$this->namespace = 'v1/pbpark';
+		$this->rest_base = 'terms';
+	}
+
+	/**
+	 * Register the REST API routes.
+	 */
+	public function register_routes() {
+
+		register_rest_route( $this->namespace, '/' . $this->rest_base . '/(?P<name>.+)', array(
+			array(
+				'methods' => WP_REST_Server::READABLE,
+				'callback' => array( $this, 'get_term' ),
+			)
+		) );
+	}
+
+	/**
+	 * Get single term
+	 * @param WP_REST_Request $request
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public static function get_term( $request ) {
+
+		$term_name = $request->get_param('name');
+
+		$term = get_term_by('slug', $term_name, $request->get_param('taxonomy') ?: 'category');
+
+		$image = get_field('image', $term);
+
+		$term->image = $image;
+
+		return rest_ensure_response($term);
+
+	}
+
+}
